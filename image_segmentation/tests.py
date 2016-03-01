34897c52cd905654f100e9e3873bca123a0e969f
Alex Louden <alex@louden.com>
2016-03-01 21:38:48 +0800

    Basic tests

--- image_segmentation/tests.py
+++ image_segmentation/tests.py
@@ -12,9 +12,50 @@ class MyTest(TestCase):
         return app
 
     def test_root(self):
-        r = self.client.get('/test')
-        print r.data
-        assert r.status_code == 400
+        r = self.client.get('/')
+        self.assertEquals(
+            r.json, {
+                'success': False,
+                'errors': ["Image URL is missing"]
+            }
+        )
+        self.assert_400(r)
+
+    def test_valid_image(self):
+        url = 'https://httpbin.org/image/jpeg'
+        r = self.client.get('/' + url)
+        self.assert_200(r)
+
+    def test_image_500(self):
+        url = 'http://httpbin.org/status/500'
+        r = self.client.get('/' + url)
+        self.assert_400(r)
+        self.assertEquals(
+            r.json, {
+                'success': False,
+                'errors': ["URL returned a 500 status code"]
+            }
+        )
+
+    def test_file_url(self):
+        r = self.client.get('/file://blah.jpg')
+        self.assertEquals(
+            r.json, {
+                'success': False,
+                'errors': ["URL is not a valid image"]
+            }
+        )
+        self.assert_400(r)
+
+    def test_invalid_url(self):
+        r = self.client.get('/invalid_url')
+        self.assertEquals(
+            r.json, {
+                'success': False,
+                'errors': ["Image URL is invalid"]
+            }
+        )
+        self.assert_400(r)
 
 
 if __name__ == '__main__':
